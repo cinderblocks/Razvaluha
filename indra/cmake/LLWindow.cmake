2225633930d15d264be4714a098e5ba41cc71ebc
Cinder <cinder@cinderblocks.biz>
2022-07-27 06:44:46 -0500

    Stragglers

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -1,25 +1,12 @@
 # -*- cmake -*-
 
 include(OpenGL)
-
-if (LINUX)
-  include(FindSDL)
-
-  # This should be done by FindSDL.  Sigh.
-  mark_as_advanced(
-      SDLMAIN_LIBRARY
-      SDL_INCLUDE_DIR
-      SDL_LIBRARY
-      )
-endif (LINUX)
-
-if (SDL_FOUND)
-  add_definitions(-DLL_SDL=1)
-  include_directories(${SDL_INCLUDE_DIR})
-endif (SDL_FOUND)
+include(GLEXT)
+include(SDL2)
 
 set(LLWINDOW_INCLUDE_DIRS
     ${GLEXT_INCLUDE_DIR}
+    ${SDL_INCLUDE_DIRS}
     ${LIBS_OPEN_DIR}/llwindow
     )
 
