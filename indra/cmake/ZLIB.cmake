6d42615ff66fdb28c180945ea32c091594671d11
Cinder <cinder@cinderblocks.biz>
2022-07-27 14:19:17 -0500

    Well this is getting pretty annoying. Add minizip. Why? Who tf knows?

--- indra/cmake/ZLIB.cmake
+++ indra/cmake/ZLIB.cmake
@@ -10,11 +10,18 @@ if (STANDALONE)
   include(FindZLIB)
 else (STANDALONE)
   use_prebuilt_binary(zlib-ng)
+  use_prebuilt_binary(minizip-ng)
   if (WINDOWS)
+    set(MINIZIP_LIBRARIES 
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/libminizip.lib
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libminizip.lib)
+
     set(ZLIB_LIBRARIES 
       debug ${ARCH_PREBUILT_DIRS_DEBUG}/zlibd.lib
       optimized ${ARCH_PREBUILT_DIRS_RELEASE}/zlib.lib)
   elseif (LINUX)
+    set(MINIZIP_LIBRARIES minizip)
+    #
     # When we have updated static libraries in competition with older
     # shared libraries and we want the former to win, we need to do some
     # extra work.  The *_PRELOAD_ARCHIVES settings are invoked early
@@ -29,6 +36,7 @@ else (STANDALONE)
     set(ZLIB_PRELOAD_ARCHIVES -Wl,--whole-archive z -Wl,--no-whole-archive)
     set(ZLIB_LIBRARIES z)
   elseif (DARWIN)
+    set(MINIZIP_LIBRARIES minizip)
     set(ZLIB_LIBRARIES -Wl,-force_load,${ARCH_PREBUILT_DIRS_RELEASE}/libz.a)
   endif (WINDOWS)
   set(ZLIB_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include/zlib)
