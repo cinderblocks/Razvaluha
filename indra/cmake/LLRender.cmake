d07c8e7626bc98b72bf6f84ccedf07386b23e8d7
Cinder <cinder@cinderblocks.biz>
2022-08-01 11:27:47 -0500

    Linux shtuff

--- indra/cmake/LLRender.cmake
+++ indra/cmake/LLRender.cmake
@@ -2,11 +2,20 @@
 
 include(FreeType)
 
+if(LINUX OR STANDALONE)
+set(OPENGL_FIND_QUIETLY OFF)
+set(OPENGL_FIND_REQUIRED ON)
+set(OpenGL_GL_PREFERENCE LEGACY)
+include(FindOpenGL)
+endif(LINUX OR STANDALONE)
+
 set(LLRENDER_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llrender
+    ${OPENGL_INCLUDE_DIR}
     )
 
 set(LLRENDER_LIBRARIES
     llrender
+    ${OPENGL_LIBRARIES}
     )
 
